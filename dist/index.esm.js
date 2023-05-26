9417091107cf427867896e5d89c28295fb8804c2
Brian S Memmen <brian.memmen@gmail.com>
2023-05-25 23:05:24 -0400

    .

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -252,7 +252,7 @@ const Item = ({ item, primaryColor }) => {
         const baseClass = "block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded";
         const selectedClass = isSelected
             ? `text-white ${bgColor}`
-            : `text-gray-500 ${bgHoverColor} ${textHoverColor}`;
+            : `text-gray-500 hover:text-white hover:${bgColor}`;
         return classNames && classNames.listItem
             ? classNames.listItem({ isSelected })
             : `${baseClass} ${selectedClass}`;
